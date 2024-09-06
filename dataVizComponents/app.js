5a2094dd093914f6de6aa1227d70c09a84cd48c6
Alivki <iverlindholm@icloud.com>
2024-09-06 14:10:19 +0200

    changes to script

--- dataVizComponents/app.js
+++ dataVizComponents/app.js
@@ -23,3 +23,48 @@ function calculateCenter() {
     // centerY = (window.innerHeight - chartContainer.offsetTop) + (chartContainer.offsetHeight/2)
     // console.log(chartContainer.getBoundingClientRect().top);
 }
+
+
+
+
+
+
+
+// const conatiner  = document.getElementById("divTest")
+
+// const left  = conatiner.getBoundingClientRect().left;
+// const width  = conatiner.getBoundingClientRect().right;
+// const centerX = left + ((width-left)/2);
+
+// const tops  = conatiner.getBoundingClientRect().top;
+// const height  = conatiner.getBoundingClientRect().bottom;
+// const centerY =  tops + ((height-tops)/2);
+
+// const dot =  document.createElement("div");
+// document.body.appendChild(dot);
+// dot.id = "dot";
+// dot.style.left = `${centerX-2.5}px`;
+// dot.style.top = `${centerY-2.5}px`;
+
+// const r = 200;
+// const random = Math.floor(Math.random() * (360 - 0) + 0)
+// console.log(random);
+
+// const pointX = r * Math.sin(random)
+// const pointY = r * Math.cos(random);
+
+// const dot2 =  document.createElement("div");
+// document.body.appendChild(dot2);
+// dot2.id = "dot";
+// dot2.style.left = `${(centerX-2.5)+pointX}px`;
+// dot2.style.top = `${(centerY-2.5)+pointY}px`;
+
+// const secondPos = random + (360 / 2)
+// const pointX2 = r * Math.sin(secondPos)
+// const pointY2 = r * Math.cos(secondPos);
+
+// const dot3 =  document.createElement("div");
+// document.body.appendChild(dot3);
+// dot3.id = "dot";
+// dot3.style.left = `${(centerX-2.5)+pointX2}px`;
+// dot3.style.top = `${(centerY-2.5)+pointY2}px`;
