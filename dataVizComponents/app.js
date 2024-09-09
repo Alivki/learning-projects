56d3e6ce25b33962b2e5b700ce0c72c537042f93
Alivki <iverlindholm@icloud.com>
2024-09-09 21:59:17 +0200

    more work

--- dataVizComponents/app.js
+++ dataVizComponents/app.js
@@ -1,70 +1,76 @@
 window.onload = () => {
-    draw();  
-    const chartContainer = document.getElementById("pie");
-    centerX = window.scrollX + chartContainer.getBoundingClientRect().left;
-    console.log(chartContainer.getBoundingClientRect());
-    centerY =  "567px";
+    // const chartContainer = document.getElementById("chartContainer");
 
-    const dot = document.createElement("dot");
-    document.body.appendChild(dot);
-    dot.id = "dot";
-    dot.style.left = "200px";
-    dot.style.top = centerY;
+    draw();  
 }
 
 function draw() {
-  
+    const values = [0,20,35,30,15];
+
+    if(document.getElementById("dot") == null) { 
+        for(i = 0; i < values.length; i++) {
+            const points = calculatePointPositions(values, i);
+
+            const dot = document.createElement("div");
+            document.body.appendChild(dot);
+            dot.id = "dot";
+            dot.style.left = `${points.x}px`;
+            dot.style.top = `${points.y}px`; 
+            const randomColor = Math.floor(Math.random()*16777215).toString(16);
+            dot.style.backgroundColor = "#" + randomColor;
+            console.log("#" + randomColor)
+        }
+
+
+        // forEach.prosentageValues((e) => {
+        // });
+
+
+    } else {
+        document.getElementById("dot").remove();
+        draw();
+    }
+}
+
+function calculatePointPositions(values, i) {
+    const center = calculateCenter();
+    const prosentage = calculateProsentage(values);
+
+    console.log(values, i, center, prosentage[i], prosentage)
+
+    const r = 200;
+    // const random = Math.floor(Math.random() * 360);
+    const random = 0;
+
+    const pointX = center.x - (r * Math.cos(random + prosentage[i]));
+    const pointY = center.y - (r * Math.sin(random + prosentage[i]));
+    
+    return {
+        x: pointX,
+        y: pointY
+    }
+}
+
+function calculateProsentage(array) {
+    const radianOfCircle = [];
+
+    array.forEach(e => {
+        radianOfCircle[array.indexOf(e)] = (e * (360/100)) * (Math.PI / 180);
+    });
+   
+    return radianOfCircle;
 }
 
 function calculateCenter() {
-    const chartContainer = document.getElementById("pie");
+    const centerX = chartContainer.getBoundingClientRect().left + ((chartContainer.getBoundingClientRect().right - chartContainer.getBoundingClientRect().left) / 2);
+    const centerY = chartContainer.getBoundingClientRect().top + ((chartContainer.getBoundingClientRect().bottom - chartContainer.getBoundingClientRect().top) / 2);
 
-    // centerX = (window.innerWidth /2)
-    // centerY = (window.innerHeight - chartContainer.offsetTop) + (chartContainer.offsetHeight/2)
-    // console.log(chartContainer.getBoundingClientRect().top);
+    return {
+        x: centerX.toString(),
+        y: centerY.toString()
+    }
 }
 
-
-
-
-
-
-
-// const conatiner  = document.getElementById("divTest")
-
-// const left  = conatiner.getBoundingClientRect().left;
-// const width  = conatiner.getBoundingClientRect().right;
-// const centerX = left + ((width-left)/2);
-
-// const tops  = conatiner.getBoundingClientRect().top;
-// const height  = conatiner.getBoundingClientRect().bottom;
-// const centerY =  tops + ((height-tops)/2);
-
-// const dot =  document.createElement("div");
-// document.body.appendChild(dot);
-// dot.id = "dot";
-// dot.style.left = `${centerX-2.5}px`;
-// dot.style.top = `${centerY-2.5}px`;
-
-// const r = 200;
-// const random = Math.floor(Math.random() * (360 - 0) + 0)
-// console.log(random);
-
-// const pointX = r * Math.sin(random)
-// const pointY = r * Math.cos(random);
-
-// const dot2 =  document.createElement("div");
-// document.body.appendChild(dot2);
-// dot2.id = "dot";
-// dot2.style.left = `${(centerX-2.5)+pointX}px`;
-// dot2.style.top = `${(centerY-2.5)+pointY}px`;
-
-// const secondPos = random + (360 / 2)
-// const pointX2 = r * Math.sin(secondPos)
-// const pointY2 = r * Math.cos(secondPos);
-
-// const dot3 =  document.createElement("div");
-// document.body.appendChild(dot3);
-// dot3.id = "dot";
-// dot3.style.left = `${(centerX-2.5)+pointX2}px`;
-// dot3.style.top = `${(centerY-2.5)+pointY2}px`;
+window.addEventListener("resize", () => {
+    draw();
+})
