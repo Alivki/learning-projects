6b322ead529ebccc79cd23216e58857690f1775c
Alivki <iverlindholm@icloud.com>
2024-09-18 13:18:30 +0200

    Added hover animation and information card on hover

--- dataVizComponents/pie.js
+++ dataVizComponents/pie.js
@@ -4,9 +4,11 @@ window.onload = () => {
 }
 
 function draw(rangeInput) {
-    if(document.getElementById("path")) {
-    document.getElementById("path").remove();
-    }
+    // console.log(document.getElementsByTagName("path"))
+    // if(document.getElementsByTagName("path").length != 0) {
+    //     console.log("adw"); 
+    //     document.getElementsByTagName("path").remove();
+    // }
 
     let valuesArray = calculetValues(rangeInput);
 
@@ -20,19 +22,21 @@ function draw(rangeInput) {
 
     const radius = (window.innerWidth * 0.125) / 2;
     const outsideRadius = (window.innerHeight * 0.25) / 2;
+    const hoverRadius = (window.innerHeight * 0.275) / 2;
     
     if (valuesArray.length == 1) {
         valuesArray[0] = valuesArray[0] - 0.01;
     }
 
     for(i = 0; i < valuesArray.length; i++) {
-        const points = calculatePoints(valuesArray, cx, cy, radius, outsideRadius, i);
+        const points = calculatePoints(valuesArray, cx, cy, radius, outsideRadius, hoverRadius, i);
 
         const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
         path.id = "path";
-        svg.appendChild(path);
+        document.getElementById("chartContainer").appendChild(path);
         const randomColor = Math.floor(Math.random()*16777215).toString(16);
         path.setAttribute("fill", `#${randomColor}`);
+        path.setAttribute("prosentage", `${valuesArray[i].toFixed(2)}`)
 
         const largeArcFlag = (points.endAngle - points.startAngle > Math.PI) ? 1 : 0;
         
@@ -44,7 +48,16 @@ function draw(rangeInput) {
             `L ${points.l3} ${points.l4} `
         ].join(' ');
 
+        const hoverPathData = [
+            `M ${points.x1} ${points.y1}`,
+            `A ${radius} ${radius} 0 ${largeArcFlag} 1 ${points.x2} ${points.y2}`,
+            `L ${points.ll1} ${points.ll2}`,
+            `A ${radius + hoverRadius} ${radius + hoverRadius} 0 ${largeArcFlag} 0 ${points.xx3} ${points.yy3}`,
+            `L ${points.l3} ${points.l4} `
+        ].join(' ');
+
         path.setAttribute('d', pathData);
+        path.setAttribute("hover", hoverPathData);
     }
 }
 
@@ -104,7 +117,7 @@ function setSliderValues(amountOfValues) {
     // }
 }
 
-function calculatePoints(valuesArray, cx, cy, radius, outsideRadius, i) {
+function calculatePoints(valuesArray, cx, cy, radius, outsideRadius, hoverRadius, i) {
     const radians = calulateRadians(valuesArray);
 
     let startAngle = 0;
@@ -128,14 +141,20 @@ function calculatePoints(valuesArray, cx, cy, radius, outsideRadius, i) {
 
     const l1 = parseFloat(cx) + ((radius + outsideRadius) * Math.cos(endAngle));
     const l2 = parseFloat(cy) + ((radius + outsideRadius) * Math.sin(endAngle))
+
+    const ll1 = parseFloat(cx) + ((radius + hoverRadius) * Math.cos(endAngle));
+    const ll2 = parseFloat(cy) + ((radius + hoverRadius) * Math.sin(endAngle))
     
     const x3 = parseFloat(cx) + ((radius + outsideRadius) * Math.cos(startAngle));
     const y3 = parseFloat(cy) + ((radius + outsideRadius) * Math.sin(startAngle));
 
+    const xx3 = parseFloat(cx) + ((radius + hoverRadius) * Math.cos(startAngle));
+    const yy3 = parseFloat(cy) + ((radius + hoverRadius) * Math.sin(startAngle));
+
     const l3 = parseFloat(cx) + ((radius) * Math.cos(startAngle));
     const l4 = parseFloat(cy) + ((radius) * Math.sin(startAngle));
 
-    return {startAngle, endAngle, x1, y1, x2, y2, l1, l2, x3, y3, l3, l4}
+    return {startAngle, endAngle, x1, y1, x2, y2, l1, l2, x3, y3, l3, l4, ll1, ll2, xx3, yy3}
 }
 
 function calculetValues(rangeInput) {
@@ -164,6 +183,7 @@ function calulateRadians(valuesArray) {
 //     console.log("aølwidj")
 // }
 
+
 const amountOfValues = document.getElementById("amountOfValues");
 amountOfValuesText.textContent = amountOfValues.value;
 amountOfValues.addEventListener("input", (e) => {
@@ -177,3 +197,42 @@ amountOfValues.addEventListener("input", (e) => {
 window.addEventListener("resize", () => {
     draw(amountOfValues.value);
 });
+
+let path = "";
+document.getElementById("chartContainer").addEventListener("mouseover", (e) => {
+    if(e.srcElement.nodeName === "path") {
+        const hoverElement = document.getElementById("hiddenHover");
+        const hoverColorElement = document.getElementById("hiddenHoverColor");
+        hoverElement.style.visibility = "visible";
+        hoverColorElement.style.visibility = "visible";
+        hoverColorElement.style.backgroundColor = `${e.srcElement.getAttribute("fill")}`;
+        hoverElement.textContent = `${e.srcElement.getAttribute("prosentage")}%`;
+
+        path = e.srcElement.getAttribute("d");
+        let hoverPath = e.srcElement.getAttribute("hover");
+        e.srcElement.setAttribute("d", `${hoverPath}`);
+    }
+    e.stopPropagation();
+});
+
+document.getElementById("chartContainer").addEventListener("mouseout", (e) => {
+    if(e.fromElement.nodeName === "path") {
+        document.getElementById("hiddenHover").style.visibility = "hidden";
+        document.getElementById("hiddenHoverColor").style.visibility = "hidden";
+        e.srcElement.setAttribute("d", `${path}`);
+    }
+    e.stopPropagation();
+});
+
+document.addEventListener("mousemove", (e) => {
+    const hidden = document.getElementById("hiddenHover");
+    const hiddenColor = document.getElementById("hiddenHoverColor");
+    const x = e.clientX;
+    const y = e.clientY;
+
+    hidden.style.left = `${x - (hidden.clientWidth / 2)}px`;
+    hidden.style.top = `${y - (hidden.clientHeight + 15)}px`;
+
+    hiddenColor.style.left = `${x - (hidden.clientWidth / 2.5)}px`;
+    hiddenColor.style.top = `${y - (hidden.clientHeight + 7.5)}px`;
+})
